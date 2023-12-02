be5a581a15e80caa6e141085de4f3e47c3e12d32
Theron Humiston <theronhumiston@gmail.com>
2023-12-02 12:45:16 -0600

    day 1 refactor

--- day1/solution.py
+++ day1/solution.py
@@ -1,44 +1,42 @@
 import re
-
-MAPPING = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-}
+from functools import reduce
 
 
-def parse():
+def parse() -> [str]:
     with open('input.txt') as f:
         return f.readlines()
 
 
-def part_one():
-    data = parse()
-    total = 0
-    for line in data:
+def part_one(data: [str]) -> int:
+    def parse_line(total: int, line: str) -> int:
         digits = re.findall("\d", line)
-        total += int(f"{digits[0]}{digits[-1]}")
+        return total + int(f"{digits[0]}{digits[-1]}")
 
-    print(total)
+    return reduce(parse_line, data, 0)
 
 
-def part_two():
-    regexes = ['\d']
-    for k, _v in MAPPING.items():
-        regexes.append(k)
+def part_two(data: [str]) -> int:
+    MAPPING = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
 
+    regexes = ['\d'] + [*MAPPING.keys()]
     regex = '(?=(' + '|'.join(regexes) + '))'
-    data = parse()
 
-    total = 0
-    for line in data:
+    def parse_line(total: int, line: str) -> int:
         digits = re.findall(regex, line)
-        total += int(''.join([str(MAPPING.get(digits[i], digits[i])) for i in [0, -1]]))
+        return total + int(''.join([str(MAPPING.get(digits[i], digits[i])) for i in [0, -1]]))
 
-    print(total)
+    return reduce(parse_line, data, 0)
+
+
+print(part_one(parse()))
+print(part_two(parse()))
